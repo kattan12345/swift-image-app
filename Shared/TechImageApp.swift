f8e47403f7dfc1e4170cb2e3b05028ada3172b8a
katsutakeiichi <renoir5d@gmail.com>
2022-12-03 21:19:26 +0900

    Launching project

--- Shared/TechImageApp.swift
+++ Shared/TechImageApp.swift
@@ -9,9 +9,20 @@ import SwiftUI
 
 @main
 struct TechImageApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
+            // ContentViewを最初の画面に設定
             ContentView()
         }
+    }
+}
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        
+        // アプリ起動時に最初に呼ばれる
+        return true
     }
 }
