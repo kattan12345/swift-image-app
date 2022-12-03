116c79e0971f763e436c0acef12d1cab25c09b08
katsutakeiichi <renoir5d@gmail.com>
2022-12-03 20:44:39 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 勝田啓一 on 2022/12/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/TechImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechImageApp.swift
+//  Shared
+//
+//  Created by 勝田啓一 on 2022/12/03.
+//
+
+import SwiftUI
+
+@main
+struct TechImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
